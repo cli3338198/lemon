bd59178457fe19531567fd49962b8321c7b32723
cli3338198 <cli3338198@gmail.com>
2024-07-30 11:31:13 -0700

    changed webhook to POST only

--- src/pages/api/runCode.ts
+++ src/pages/api/runCode.ts
@@ -2,22 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 // This hack works!
 const ivm = eval("require")("isolated-vm");
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    try {
-      const { code } = req.body;
-      const isolate = new ivm.Isolate({ memoryLimit: 128 });
-      const context = isolate.createContextSync();
-      const script = isolate.compileScriptSync(code);
-      const result = script.runSync(context);
-      res.status(200).json({ result });
-    } catch (err: unknown) {
-      res.status(500).json({ error: "Something went wrong" });
-    }
-  } else {
-    res.status(405).json({ error: "Method not allowed" });
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { code } = req.body;
+    const isolate = new ivm.Isolate({ memoryLimit: 128 });
+    const context = isolate.createContextSync();
+    const script = isolate.compileScriptSync(code);
+    const result = script.runSync(context);
+    res.status(200).json({ result });
+  } catch (err: unknown) {
+    res.status(500).json({ error: "Something went wrong" });
   }
 }
